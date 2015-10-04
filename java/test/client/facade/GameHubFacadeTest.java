abeb2fd041c5f810f2d5dd8f1503c46f9a1d1d9d
Matthew Shurtz <shurtzm@gmail.com>
2015-10-03 22:13:07 -0600

    mode deleted test.

--- java/test/client/facade/GameHubFacadeTest.java
+++ java/test/client/facade/GameHubFacadeTest.java
@@ -74,29 +74,29 @@ public class GameHubFacadeTest {
         fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of addAI method, of class GameHubFacade.
-     */
-    @Test
-    public void testAddAI() {
-        System.out.println("addAI");
-        GameHubFacade instance = null;
-        instance.addAI();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    /**
+//     * Test of addAI method, of class GameHubFacade.
+//     */
+//    @Test
+//    public void testAddAI() {
+//        System.out.println("addAI");
+//        GameHubFacade instance = null;
+//        instance.addAI();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
-    /**
-     * Test of listAI method, of class GameHubFacade.
-     */
-    @Test
-    public void testListAI() {
-        System.out.println("listAI");
-        GameHubFacade instance = null;
-        instance.listAI();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    /**
+//     * Test of listAI method, of class GameHubFacade.
+//     */
+//    @Test
+//    public void testListAI() {
+//        System.out.println("listAI");
+//        GameHubFacade instance = null;
+//        instance.listAI();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of createGame method, of class GameHubFacade.
@@ -110,43 +110,43 @@ public class GameHubFacadeTest {
         fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of join method, of class GameHubFacade.
-     */
-    @Test
-    public void testJoin() {
-        System.out.println("join");
-        GameInfo gameInfo = null;
-        String color = "";
-        GameHubFacade instance = null;
-        instance.join(gameInfo, color);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    /**
+//     * Test of join method, of class GameHubFacade.
+//     */
+//    @Test
+//    public void testJoin() {
+//        System.out.println("join");
+//        GameInfo gameInfo = null;
+//        String color = "";
+//        GameHubFacade instance = null;
+//        instance.join(gameInfo, color);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
-    /**
-     * Test of save method, of class GameHubFacade.
-     */
-    @Test
-    public void testSave() {
-        System.out.println("save");
-        GameHubFacade instance = null;
-        instance.save();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of load method, of class GameHubFacade.
-     */
-    @Test
-    public void testLoad() {
-        System.out.println("load");
-        GameHubFacade instance = null;
-        instance.load();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    /**
+//     * Test of save method, of class GameHubFacade.
+//     */
+//    @Test
+//    public void testSave() {
+//        System.out.println("save");
+//        GameHubFacade instance = null;
+//        instance.save();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of load method, of class GameHubFacade.
+//     */
+//    @Test
+//    public void testLoad() {
+//        System.out.println("load");
+//        GameHubFacade instance = null;
+//        instance.load();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of login method, of class GameHubFacade.
@@ -172,16 +172,16 @@ public class GameHubFacadeTest {
         fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of changeLogLevel method, of class GameHubFacade.
-     */
-    @Test
-    public void testChangeLogLevel() {
-        System.out.println("changeLogLevel");
-        GameHubFacade instance = null;
-        instance.changeLogLevel();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    /**
+//     * Test of changeLogLevel method, of class GameHubFacade.
+//     */
+//    @Test
+//    public void testChangeLogLevel() {
+//        System.out.println("changeLogLevel");
+//        GameHubFacade instance = null;
+//        instance.changeLogLevel();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
     
 }
