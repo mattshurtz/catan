7baf47fbc84902f9ed6c52fafba9448efcd4cf3b
Matthew Shurtz <shurtzm@gmail.com>
2015-10-24 10:53:30 -0600

    chat is working!

--- java/src/client/communication/ChatController.java
+++ java/src/client/communication/ChatController.java
@@ -1,18 +1,27 @@
 package client.communication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
 import client.base.*;
 import client.facade.CatanFacade;
+import shared.definitions.CatanColor;
 import shared.exceptions.ServerException;
+import shared.model.MessageLine;
+import shared.model.MessageList;
 
 
 /**
  * Chat controller implementation
  */
-public class ChatController extends Controller implements IChatController {
+public class ChatController extends Controller implements IChatController, Observer {
 
 	public ChatController(IChatView view) {
 		
 		super(view);
+		CatanFacade.addObserver(this);
 	}
 
 	@Override
@@ -29,6 +38,29 @@ public class ChatController extends Controller implements IChatController {
             e.printStackTrace();
         }
 	}
+
+	private void initFromModel() {
+        if ( CatanFacade.getModel() == null )
+            return;
+        
+		List<LogEntry> entries = new ArrayList<LogEntry>();
+		MessageList msgs = CatanFacade.getModel().getChat();
+        for ( MessageLine msg : msgs.getLines() ) {
+            CatanColor theColor = CatanFacade.getColorFromPlayerName( msg.getSource() );
+            
+    		if ( theColor != null )
+                entries.add(new LogEntry( theColor, msg.getMessage() ));
+            else
+                System.err.println("Error while getting color from player name!");
+        }
+		
+        getView().setEntries(entries);
+	}
+
+    @Override
+    public void update(Observable o, Object arg) {
+        initFromModel();
+    }
 
 }
 
