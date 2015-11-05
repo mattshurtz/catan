1a7eac6a22da3be2432a37a96b04c8622097cc1c
Scott Brantley <sebrantley@gmail.com>
2015-11-05 09:59:50 -0700

    javadoc-ed GameInfoContainer

--- java/src/server/gameinfocontainer/GameInfoContainer.java
+++ java/src/server/gameinfocontainer/GameInfoContainer.java
@@ -10,5 +10,29 @@ package server.gameinfocontainer;
  * @author Scott
  */
 public class GameInfoContainer {
+    private ModelBank models;
+    private UserInfoBank users;
     
+    /**
+     * Constructs the GameInfoContainer Object
+     */
+    GameInfoContainer() {
+        
+    }
+    
+    /**
+     * 
+     * @return ModelBank of game models currently being handled by the server.
+     */
+    ModelBank getModels() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+  
+    /**
+     * 
+     * @return UserInfoBank of registered user information.
+     */
+    UserInfoBank getRegisteredUserInfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
