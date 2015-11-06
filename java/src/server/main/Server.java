0da8e9f6fa2f2488a0ed6d6cda4a37825cc53a58
GarrettHartley <ljgarretth@gmail.com>
2015-11-05 19:13:13 -0700

    Server Java-doc

--- java/src/server/main/Server.java
+++ java/src/server/main/Server.java
@@ -5,9 +5,79 @@
  */
 package server.main;
 
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
 /**
- *
+ *Initializes web server. Maybe this will read games from a file later (phase 4)
+ * Create Server listening on the port specified. 
  */
-public class Server {
+public class Server extends HttpServer{
+    
+    public Server(){
+        
+    }
+    
+    /**
+     * Specifies port to listen to and starts the server. 
+     * @param args port number
+     */
+    public static void main(String args[]){
+        
+    }
+
+    @Override
+    public void bind(InetSocketAddress isa, int i) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void start() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setExecutor(Executor exctr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Executor getExecutor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stop(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HttpContext createContext(String string, HttpHandler hh) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HttpContext createContext(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeContext(String string) throws IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeContext(HttpContext hc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public InetSocketAddress getAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
