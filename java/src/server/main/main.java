9b37bd05cdf511cc6855f8e6c55eee0835693964
Matthew Shurtz <shurtzm@gmail.com>
2015-11-09 12:00:59 -0700

    server2

--- java/src/server/main/main.java
+++ java/src/server/main/main.java
@@ -8,6 +8,16 @@ package server.main;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.ssl.internal.www.protocol.https.Handler;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import server.HTTPhandlers.DefaultHandler;
+import server.HTTPhandlers.GameHandler;
+import server.HTTPhandlers.GamesHandler;
+import server.HTTPhandlers.MovesHandler;
+import server.HTTPhandlers.UserHandler;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executor;
@@ -16,9 +26,10 @@ import java.util.concurrent.Executor;
  *Initializes web server. Maybe this will read games from a file later (phase 4)
  * Create Server listening on the port specified. 
  */
-public class Server extends HttpServer{
+public class main{
     
-    public Server(){
+	
+    public main(){
         
     }
     
@@ -26,58 +37,63 @@ public class Server extends HttpServer{
      * Specifies port to listen to and starts the server. 
      * @param args port number
      */
-    public static void main(String args[]){
-        
-    }
+    /**
+	 * The main function that receives the port number and starts the server
+	 * 
+	 * @param args
+	 *            The port number
+	 */
+	public static void main(String[] args) {
+	//start server with default port if no argument is given
+		if (args.length == 1) {
+			new main().run(Integer.parseInt(args[0]));
+		} else if (args.length == 0) {
+			new main().run(DEFAULT_SERVER_PORT_NUMBER);
+		}
+	}
+	private static final int DEFAULT_SERVER_PORT_NUMBER = 8081;
+	private static final int MAX_WAITING_CONNECTIONS = 10;
+	//private static final String DATA_LOCATION = "/data/";
 
-    @Override
-    public void bind(InetSocketAddress isa, int i) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	private HttpServer server;
+	
 
-    @Override
-    public void start() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	/**
+	 * The run for the HTTP server
+	 * 
+	 * @param port_number
+	 *            The port number
+	 */
+	public void run(int port_number) {
+	//initializes stuff
 
-    @Override
-    public void setExecutor(Executor exctr) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
-    @Override
-    public Executor getExecutor() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	//initializes HTTP server
+		try {
+			server = HttpServer.create(new InetSocketAddress(port_number),
+					MAX_WAITING_CONNECTIONS);
+			System.out.println("listening on port: " + port_number);
+		} catch (IOException e) {
+			System.out.println("Could not create HTTP server: "
+					+ e.getMessage());
+			e.printStackTrace();
+			return;
+		}
+		
+//	//initializes HTTP handlers
+//		Handler handlers = new Handlers();
 
-    @Override
-    public void stop(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	//specify handlers
+		server.setExecutor(null); // use the default executor
 
-    @Override
-    public HttpContext createContext(String string, HttpHandler hh) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public HttpContext createContext(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void removeContext(String string) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void removeContext(HttpContext hc) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public InetSocketAddress getAddress() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+		server.createContext("/Game", new GameHandler());
+		server.createContext("/Games", new GamesHandler());
+		server.createContext("/Moves", new MovesHandler());
+		server.createContext("/User", new UserHandler());
+		server.createContext("/", new DefaultHandler());
+		
+	//start server
+		server.start();
+	}
     
 }
