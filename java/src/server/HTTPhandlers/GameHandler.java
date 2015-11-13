a68956d3fb2dcc08a1986fc94dd9559aad37f59b
Matthew Shurtz <shurtzm@gmail.com>
2015-11-13 12:04:16 -0700

    handlers!

--- java/src/server/HTTPhandlers/GameHandler.java
+++ java/src/server/HTTPhandlers/GameHandler.java
@@ -7,17 +7,52 @@ package server.HTTPhandlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
+import shared.exceptions.HTTPBadRequest;
+
 import java.io.IOException;
+import java.net.URI;
 
 /**
  * HTTP Handler for the requests starting with /Game
  * operations on a game active on the server. 
  */
-public class GameHandler implements HttpHandler {
+public class GameHandler extends catanHTTPHandler {
 
     @Override
-    public void handle(HttpExchange he) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void handle(HttpExchange exchange) throws IOException {
+		System.out.println("CLIENT CALLED: " + exchange.getRequestURI().getPath());
+		
+		try {
+			
+			//prep command
+			URI url = exchange.getRequestURI();
+			String newCommand = "game." + url.getPath().replace("/game/", "");
+    		
+			String content = null;
+			String gameId = this.getGameId(exchange);
+			String user = this.getPlayerId(exchange);
+			
+			//if it is the list (GET)
+			this.checkisGet(exchange);
+			
+			//Call the facade
+			String result = this.sendToFacade(newCommand, content, gameId, user);
+			
+			if(result != null) {
+					//send in body as json
+					this.setJSONResponse(exchange, result);				
+			} else {
+				//login failed
+				throw new HTTPBadRequest("Failed to get list of games");
+			}
+			
+		} catch (HTTPBadRequest e) {
+			setBadRequest(exchange,e.getMessage());
+			System.out.println(e.getMessage());
+		} finally {
+			exchange.getResponseBody().close();
+		}	
     }
     
 }
