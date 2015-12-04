eeb2a99941616b25965956b8541953ee2aa3a73b
Jan Bergeson <jphbergeson@gmail.com>
2015-12-03 18:27:16 -0700

    sqlite connection test class just for funzies (not added to AllTests.java

---
+++ java/test/server/SQLiteSetupTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author JanPaul
+ */
+public class SQLiteSetupTest {
+    public static void main(String args[]) {
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:persistence/test.db");
+        } catch (Throwable e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+}
